8bb7df7deb690ce9e1bc3cf796336b77d0ec27af
Mary <njuemarywanjiru@gmail.com>
2025-04-09 11:16:41 +0100

    basic api integration

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World, Shi!');
+app.get('/joke',async (req, res) => {
+    try{
+        const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+        res.json(response.data);
+    }
+    catch (error) {
+        console.error('Error fetching joke:', error);
+        res.status(500).send('Failed to fetch joke')
+    }
 });
 
-app.get('/message', (req,res) =>{
-    res.json({type: 'message', mood:  'cozy'});
-})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
